af6ccd08c5e2881803cb8b2cc8e5363f12302c88
Lazar Nikolov <hello@nikolovlazar.com>
2023-04-19 13:54:55 -0400

    idk lots of things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import lazyLoadPlugin from "rehype-plugin-image-native-lazy-loading";
 
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+
 import { remarkReadingTime } from "./src/utils/content";
 
 // https://astro.build/config
@@ -15,7 +19,20 @@ export default defineConfig({
 
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [lazyLoadPlugin],
+    rehypePlugins: [
+      lazyLoadPlugin,
+      rehypeSlug,
+      [rehypeExternalLinks, {
+        target: "_blank",
+        rel: "external nofollow noopener",
+      }],
+      [rehypeAutolinkHeadings, {
+        behavior: "wrap",
+        properties: {
+          ariaHidden: false,
+        },
+      }],
+    ],
     extendDefaultPlugins: true,
   },
 
---
+++ src/pages/about.md
@@ -0,0 +1,22 @@
+---
+title: "About"
+layout: "../layouts/about.astro"
+---
+
+![A photo of Lazar Nikolov](/assets/lazar-1.jpg)
+
+Hey, I'm Lazar Nikolov. I'm a developer advocate at
+[Sentry](https://sentry.io/welcome). I live in Toronto, Canada, but originally
+I'm from North Macedonia. As a fullstack engineer, I enjoy building interesting
+pet projects from scratch. Of course, just like any normal developer I end up
+abandoning them. I'm a super curious fellow and I'm always up to learn something
+new. Aside from learning new things, I enjoy live streaming on YouTube and
+creating courses for [egghead.io](https://egghead.io).
+
+## How everything started
+
+Hello.
+
+![A photo of Lazar Nikolov](/assets/lazar-2.jpg)
+
+Come on.
