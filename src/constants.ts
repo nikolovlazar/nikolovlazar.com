451c1a99a6c471e83c65fe3911e821cee05fdf00
Lazar Nikolov <hello@nikolovlazar.com>
2023-04-19 21:50:18 -0400

    blog page + statically rendered pagination and tag filtering

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const YOUTUBE_URL = "https://youtube.com/@nikolovlazar";
 export const TWITTER_URL = "https://twitter.com/nikolovlazar";
 export const GITHUB_URL = "https://github.com/nikolovlazar";
 export const POLYWORK_URL = "https://polywork.com/nikolovlazar";
+export const POSTS_PER_PAGE = 10;
