104a9c0fa6e5dcf36fdc698bbd9fba4ffd588c32
Lazar Nikolov <hello@nikolovlazar.com>
2023-04-23 17:08:15 -0400

    vercel integration + updated 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,50 +1,39 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import lazyLoadPlugin from 'rehype-plugin-image-native-lazy-loading'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import lazyLoadPlugin from 'rehype-plugin-image-native-lazy-loading';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import rehypeCodeBlockEnhancer from './src/markdown-plugins/rehype-code-block-enhancer';
+import remarkReadingTime from './src/markdown-plugins/remark-reading-time';
 
-import rehypeExternalLinks from 'rehype-external-links'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
-
-import rehypeCodeBlockEnhancer from './src/markdown-plugins/rehype-code-block-enhancer'
-import remarkReadingTime from './src/markdown-plugins/remark-reading-time'
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nikolovlazar.com',
-
-    experimental: {
-        contentCollections: true,
-        assets: true,
-    },
-
-    markdown: {
-        remarkPlugins: [remarkReadingTime],
-        rehypePlugins: [
-            rehypeCodeBlockEnhancer,
-            lazyLoadPlugin,
-            rehypeSlug,
-            [
-                rehypeExternalLinks,
-                {
-                    target: '_blank',
-                    rel: 'external nofollow noopener',
-                },
-            ],
-            [
-                rehypeAutolinkHeadings,
-                {
-                    behavior: 'wrap',
-                    properties: {
-                        ariaHidden: false,
-                    },
-                },
-            ],
-        ],
-        extendDefaultPlugins: true,
-        syntaxHighlight: 'prism',
-    },
-
-    integrations: [mdx(), sitemap()],
-})
+  site: 'https://nikolovlazar.com',
+  experimental: {
+    contentCollections: true,
+    assets: true
+  },
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [rehypeCodeBlockEnhancer, lazyLoadPlugin, rehypeSlug, [rehypeExternalLinks, {
+      target: '_blank',
+      rel: 'external nofollow noopener'
+    }], [rehypeAutolinkHeadings, {
+      behavior: 'wrap',
+      properties: {
+        ariaHidden: false
+      }
+    }]],
+    extendDefaultPlugins: true,
+    syntaxHighlight: 'prism'
+  },
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
+});
