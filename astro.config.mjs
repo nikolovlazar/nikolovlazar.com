eb4ddc08bb026e535dd9cddd8fa888dbe7563d75
Lazar Nikolov <hello@nikolovlazar.com>
2023-04-23 21:39:36 -0400

    ignoring vercel dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,50 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import lazyLoadPlugin from 'rehype-plugin-image-native-lazy-loading';
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import rehypeCodeBlockEnhancer from './src/markdown-plugins/rehype-code-block-enhancer';
-import remarkReadingTime from './src/markdown-plugins/remark-reading-time';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import lazyLoadPlugin from 'rehype-plugin-image-native-lazy-loading'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import rehypeCodeBlockEnhancer from './src/markdown-plugins/rehype-code-block-enhancer'
+import remarkReadingTime from './src/markdown-plugins/remark-reading-time'
 
-import vercel from "@astrojs/vercel/static";
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nikolovlazar.com',
   experimental: {
-    contentCollections: true,
-    assets: true
+    assets: true,
   },
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [rehypeCodeBlockEnhancer, lazyLoadPlugin, rehypeSlug, [rehypeExternalLinks, {
-      target: '_blank',
-      rel: 'external nofollow noopener'
-    }], [rehypeAutolinkHeadings, {
-      behavior: 'wrap',
-      properties: {
-        ariaHidden: false
-      }
-    }]],
+    rehypePlugins: [
+      rehypeCodeBlockEnhancer,
+      lazyLoadPlugin,
+      rehypeSlug,
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: 'external nofollow noopener',
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          properties: {
+            ariaHidden: false,
+          },
+        },
+      ],
+    ],
     extendDefaultPlugins: true,
-    syntaxHighlight: 'prism'
+    syntaxHighlight: 'prism',
   },
   integrations: [mdx(), sitemap()],
-  output: "static",
+  output: 'static',
   adapter: vercel({
-    analytics: true
-  })
-});
+    analytics: true,
+  }),
+})
