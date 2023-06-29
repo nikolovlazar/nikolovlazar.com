3c1da316294768149a2ef30c2d04bcf567446a0b
Lazar Nikolov <hello@nikolovlazar.com>
2023-06-29 19:22:14 -0400

    added twitch link

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,5 @@ export const YOUTUBE_URL = 'https://youtube.com/@nikolovlazar'
 export const TWITTER_URL = 'https://twitter.com/nikolovlazar'
 export const GITHUB_URL = 'https://github.com/nikolovlazar'
 export const POLYWORK_URL = 'https://polywork.com/nikolovlazar'
+export const TWITCH_URL = 'https://twitch.tv/nikolovlazar'
 export const POSTS_PER_PAGE = 10
